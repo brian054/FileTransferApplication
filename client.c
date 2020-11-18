3b4733e1231480cd18f2f1f5bfb54b48de6f2b6e
brian054 <brian.rowell54@gmail.com>
2020-11-18 12:21:33 -0500

    Final client.c and server.c files

--- client.c
+++ client.c
@@ -3,7 +3,10 @@
     Author: Brian Rowell
     Section 1
 
-    This is the server program. 
+    This is the client program. 
+
+    Note: There are citations in the code itself, but I also include
+    references in my README.md file.
 
 */
 #include <stdio.h>
@@ -14,20 +17,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <libgen.h> // for extracting file name https://linux.die.net/man/3/basename
-/*
-
-    Questions:
-        Error control for path that specifies a directory instead of a file?
-
-
-        https://linux.die.net/man/2/access
-
-*/
-
-// Bug happens when you delete file before running server
-// Bug happens when you run server then delete file
-
+#include <libgen.h> 
 
 // This function is called when a system call fails
 // It prints a message and then terminates the program
@@ -40,8 +30,7 @@ void error(char *s) {
 // Main
 int main(int argc, char *argv[]) {
 
-    // This variable is a file descriptor that stores the value returned by the 
-    // socket system call
+    // sockfd variable is a file descriptor that stores the value returned by the socket system call
     // n = number of character written or read
     int sockfd, port_number = 2001, n;
     struct sockaddr_in server_address;
@@ -55,34 +44,40 @@ int main(int argc, char *argv[]) {
     }
 
     // Check and verify file path is valid
+    // I use access() here instead of fopen() due to an unknown error that kept popping up in my file transfers
+    // when opening the file here. access() checks whether the process can access the file in file_path
+    // Citation: https://linux.die.net/man/2/access
     char *file_path = argv[2];
-    // Explain why you used this instead of just fopen()
     if (access(file_path, F_OK) != 0) {
         error("ERROR");
     }
     
     // Extract the file name from the path given by the user
-    // https://www.systutorials.com/how-to-get-the-directory-path-and-file-name-from-a-absolute-path-in-c-on-linux/
     char *file_name = basename(strdup(file_path));
-
+    
+    // Create a new socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         error("ERROR opening socket");
     }
 
-    //server = gethostbyaddr(argv[1], sizeof(argv[1]), AF_INET);
+    // You could also use 
+    // server = gethostbyaddr(argv[1], sizeof(argv[1]), AF_INET);
+    // Retrieve server name
     server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr, "ERROR, no such host \n");
         exit(0);
     }
 
+    // Set the fields in serv_addr
     bzero((char *) &server_address, sizeof(server_address));
     server_address.sin_family = AF_INET;
     bcopy((char *) server->h_addr, (char *) &server_address.sin_addr.s_addr
                 , server->h_length);
     server_address.sin_port = htons(port_number);
 
+    // Establish connection to server
     if (connect(sockfd, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
         error("ERROR connecting");
     }
@@ -90,52 +85,32 @@ int main(int argc, char *argv[]) {
     // At this point we have connected to server
     printf("Connected to Server.... \n");
 
-    // Give Server name of file
-    //char file_name_buffer[strlen(file_name)];
-    //bzero(file_name_buffer, strlen(file_name));
-    
-    // Put Name into Buffer
-    //sprintf(file_name_buffer, "%s \n", file_name);
-    //printf("File Name: %s \n", file_name_buffer);
-    // write what's in buffer to server
+    // Give the server the name of the file
     int file_size_characters = strlen(file_name);
     int converted_file_size = htonl(file_size_characters);
-
     write(sockfd, &converted_file_size, sizeof(converted_file_size));
     write(sockfd, file_name, strlen(file_name));
     
-    // If this stays, explain bug that was happening and why you put this here
+    // Open our file to send
     FILE *myfile = fopen(file_path, "r");
     if (!myfile) {
         error("ERROR");
     }  
 
-    //bzero(buffer, 256); //was 255
-
-    // while not at end of file
+    // while we have not reached end of file
     while (!feof(myfile)) {
         // Clear the buffer
         bzero(buffer, 256);
         // read a line from file and store it in buffer
         fgets(buffer, 256, myfile);
-        //printf("Yup: %s\n", buffer);
         // write that line to server
         n = write(sockfd, buffer, strlen(buffer));
         if (n < 0) {
             error("ERROR writing to socket");
         }
-        /*
-        bzero(buffer, 256);
-        n = read(sockfd, buffer, 256);
-        if (n < 0) {
-        error("ERROR reading from socket");
-        }
-        printf("%s\n", buffer);
-        */   
     }
+    // Close the file once finished writing
     fclose(myfile);
-    // So we can either have the server send us back the file and we confirm its correct
-    // or we can just specify that sometimes it screws up but not that often
 
     printf("File Sent.... \n");
     printf("Connection ended.... \n");
--- server.c
+++ server.c
@@ -5,19 +5,19 @@
 
     This is the server program. 
 
+    Note: There are citations in the code itself, but I also include
+    references in my README.md file.
+
 */
 #include <stdio.h>
-#include <sys/types.h> // def's of a number of data types used in system calls, these
-                            // types are used in the next two include
-#include <sys/socket.h> // def's of structures needed for sockets
-#include <netinet/in.h> // constants and structures needed for internet domain addresses
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <netinet/in.h> 
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-// put comments above in a read me bruh
-// Remove any naked literals
+#include <dirent.h> 
 
 // This function is called when a system call fails
 // It prints a message and then terminates the program
@@ -27,14 +27,26 @@ void error(char *s) {
     exit(1);
 } // error()
 
+// Create file path in current directory if not already there
+void createOutputDirectory() {
+    DIR *directory = opendir("output");
+    if (!directory) {
+        system("mkdir output"); 
+    } else {
+        closedir(directory);
+    }
+} // createOutputDirectory()
+
 // Main
 int main(int argc, char *argv[]) {
 
+    // Note: port_number is just a random number between 2000 and 65535, 
+    // the server and client must use the same port number
+
     printf("Server Started.... \n");
     int sockfd, newsockfd, pid; 
-    // port number is just a random number between 2000 and 65535
     int port_number = 2001; // Stores the port number on which the server accepts connections
-    int client_address_length; // Stores size of address of client 
+    int client_address_length; 
     int n; // contains the number of characters read or written
 
     char buffer[256]; // server reads characters from the socket connection into this buffer
@@ -53,8 +65,7 @@ int main(int argc, char *argv[]) {
 
     // code for the address family, always set to AF_INET
     server_address.sin_family = AF_INET;
-    // htons() converts port number in host byte order to a port number
-    // in network byte order
+    // htons() converts port number in host byte order to a port number in network byte order
     server_address.sin_port = htons(port_number);
 
     // IP Address of the Host
@@ -69,10 +80,12 @@ int main(int argc, char *argv[]) {
     // Listen on the socket for connections
     listen(sockfd, 5);
 
-    // accept() causes the process to block until a client connects to server
+    // Set client address length
     client_address_length = sizeof(client_address);
 
+        // Run Server indefinitely
         while (1) {
+            // accept() causes the process to block until a client connects to server
             newsockfd = accept(sockfd, (struct sockaddr *) &client_address, 
                             (socklen_t *) &client_address_length);
             if (newsockfd < 0) {
@@ -85,8 +98,6 @@ int main(int argc, char *argv[]) {
             if (pid == 0) {
                 close(sockfd);
 
-                // Do Stuff
-
                 // We only get to this point from a successful client connection
                 printf("Client Connected.... \n");
 
@@ -96,6 +107,7 @@ int main(int argc, char *argv[]) {
                 if (n < 0) {
                     error("ERROR reading from socket");
                 }
+                // Convert that into the actual number of characters in the file name
                 int converted_size = ntohl(file_size_characters);
 
                 // Read in data and put into buffer
@@ -105,47 +117,42 @@ int main(int argc, char *argv[]) {
 
                 // To eliminate any other char's after .txt
                 file_name_buffer[converted_size] = '\0';
-
                 char *file_name = file_name_buffer;
 
-                // Remember file_name_buffer and file_name hold same value at this point
-
+                // Tell the user we are in the process of transferring the file to server
                 printf("Transferring %s to server....\n", file_name_buffer);
-                printf("Transferring %s to server....\n", file_name);
 
-                // Create a file path
-                char file_path[256] = "output/"; 
+                // Create a file path string
+                char file_path[256] = "output/";
+                // Create output file in directory if not already there
+                createOutputDirectory();
                 // Append file name to output path
                 strcat(file_path, file_name);
                 FILE *new_file = fopen(file_path, "w");
-                //printf("%s \n", file_name_buffer);
+                if (!new_file) {
+                    error("ERROR");
+                }
 
                 while (1) {
                     // Read in line from file and place into buffer
                     n = read(newsockfd, buffer, 255);
-                    //printf("%s \n", buffer);
                     if (n < 0) {
                         error("ERROR reading from socket");
                     } else if (n == 0) {
                         // n == 0 signifies we have no more characters to read in 
                         break;
                     }
-
                     // Write whatever's in buffer into new file
                     fputs(buffer, new_file);
                     bzero(buffer, 256);
                 }
-
                 printf("File Received.... \n");
                 printf("Connection Ended.... \n");
-
-                // ---------------------------------------
-
                 exit(0);
             } else {
                 close(newsockfd);
             }
         }
-    // We never get here
+    // We should never ever get here
     return 0;
 }
